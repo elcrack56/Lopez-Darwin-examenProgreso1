d06aa794becd09a1d7e519753014938b241cc095
ErickG11 <grandaerick8@gmail.com>
2024-10-30 08:35:33 -0500

    Agregar archivos de proyecto.

---
+++ Lopez-Darwin-examenProgreso1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lopez_Darwin_examenProgreso1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
